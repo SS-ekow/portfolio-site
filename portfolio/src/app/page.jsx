d3e751f65537d6719aaa86238860d95d9dfd9dbc
SS-ekow <120583131+SS-ekow@users.noreply.github.com>
2024-04-22 23:22:07 +0000

    idek

--- portfolio/src/app/page.jsx
+++ portfolio/src/app/page.jsx
@@ -1,35 +1,42 @@
+'use client';
+import { useTransform, useScroll, motion } from "framer-motion";
+import { useRef } from "react";
+
+
+
+
 export default function Home() {
+
+  const container = useRef(null);
+  const { scrollYProgress} = useScroll(
+    {
+      target: container,
+      offset: ['start end', 'end start']
+    }
+  )
+
+  const y = useTransform(scrollYProgress, [0, 1], [0, -1000])
+
   return (
     <main>
       <section className="hero">
-        <nav className="navbar">
-          <div className="logo">
+        <NavBar/>
+        <div className="division">
           
-          </div>
-          <ul className="nav-links">
-            <li>
-              <a href="#hero">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#ask">Ask</a>
-            </li>
-          </ul>
+        </div>
+        <h1>Hero Page will be added later</h1>
 
-          <button className="cta">Hire Us</button>
-
-
-        </nav>
-        <h1 className="text-2xl font-bold mx-32" >Hero Page will be added later</h1>
+        <Bubble />
       </section>
-      <section className="about" id="about">
-        <div className="col-1">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam repellat perferendis cupiditate ipsam obcaecati nihil magnam, quisquam cum eveniet, facilis laborum velit molestiae quaerat, consequatur harum? Voluptatibus ex, molestiae unde repellendus, iste non reprehenderit voluptates doloribus at ipsa totam quae. Vitae error corrupti architecto. Mollitia, sapiente maiores reiciendis nisi consequuntur veritatis illo magni aut aliquid ut debitis iusto adipisci placeat temporibus recusandae possimus dicta amet magnam distinctio, voluptas eligendi. Assumenda possimus, vero, deleniti veritatis quos ullam ex quidem adipisci molestias tempora harum omnis repellendus quisquam ut nostrum nobis minus qui est velit voluptatem? Eum, quos? Velit hic quos aperiam nisi odit, rem nobis numquam possimus sunt eum ab. Ipsa ullam ad totam perspiciatis cum harum consectetur nisi molestiae vero est eaque modi, doloremque iure excepturi exercitationem reiciendis nihil provident quis enim itaque ipsam. Debitis, odio veniam eos mollitia voluptatem consequuntur? Mollitia nostrum quibusdam veniam quis eligendi. Beatae minus est sint perferendis adipisci commodi iusto eveniet obcaecati rem! Debitis magnam culpa, officiis quaerat optio dolore eveniet iure dolorum tenetur distinctio. Ut, illum. Cumque earum ipsa, neque quidem nam accusantium quam, assumenda libero in voluptatem molestiae dolores molestias sequi possimus maxime saepe iusto reprehenderit iure voluptatibus autem ea quo facere fugiat. Aliquam.</p>
+      <section className="about" id="about" ref={container}>
+        <div className="col-1" y={y}>
+          <p > 
+          
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam repellat perferendis cupiditate ipsam obcaecati nihil magnam, quisquam cum eveniet, facilis laborum 
+          velit molestiae quaerat, consequatur harum? Voluptatibus ex, molestiae unde repellendus, iste non reprehenderit voluptates doloribus at ipsa totam quae. Vitae error corrupti architecto. Mollitia, sapiente maiores reiciendis nisi consequuntur veritatis illo magni aut aliquid ut debitis iusto adipisci placeat temporibus recusandae possimus dicta amet magnam distinctio, voluptas eligendi. 
+          Assumenda possimus, vero, deleniti veritatis quos ullam ex quidem adipisci molestias tempora harum omnis repellendus quisquam ut nostrum nobis minus qui est velit voluptatem? Eum, quos? Velit hic quos aperiam nisi odit, rem nobis numquam possimus sunt eum ab. Ipsa ullam ad totam perspiciatis cum harum consectetur nisi molestiae vero est eaque modi, doloremque iure excepturi exercitationem 
+          reiciendis nihil provident quis enim itaque ipsam. Debitis, odio veniam eos mollitia voluptatem consequuntur? Mollitia nostrum quibusdam veniam quis eligendi. Beatae minus est 
+          sint perferendis adipisci commodi iusto eveniet obcaecati rem! Debitis magnam culpa, officiis quaerat optio dolore eveniet iure dolorum tenetur distinctio. Ut, illum. Cumque earum ipsa, neque quidem nam accusantium quam, assumenda libero in voluptatem molestiae dolores molestias sequi possimus maxime saepe iusto reprehenderit iure voluptatibus autem ea quo facere fugiat. Aliquam.</ p>
         </div>
         <div className="col-2">
           <div>
@@ -99,4 +106,42 @@ export default function Home() {
       </section>
     </main>
   );
+}
+
+const NavBar = () => {
+  return(
+    <nav className="navbar">
+          <div className="logo">
+          
+          </div>
+          <ul className="nav-links">
+            <li>
+              <a href="#hero">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#ask">Ask</a>
+            </li>
+          </ul>
+
+          <button className="cta">Hire Us</button>
+
+
+    </nav>
+  )
+}
+
+const Bubble = () => {
+  return(
+    <div className="bubble">
+          <div className="stack">
+            <img src="/portfolio/public/structure.png" alt="stack" />
+          </div>
+    </div>
+  )
 }
